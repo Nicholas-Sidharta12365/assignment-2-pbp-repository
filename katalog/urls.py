33d99efd1ba0b5d35eb1f4647224599d136ba3f7
Nicholas Sidharta <toxicancm@gmail.com>
2022-09-11 19:40:01 +0700

    finish task

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
 # TODO: Implement Routings Here
+
+urlpatterns= {
+   path('', views.katalog, name="katalog") 
+}
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from katalog.models import *
 
 # TODO: Create your views here.
+
+def katalog(request):
+
+    katalog = CatalogItem.objects.all()
+
+    data = {
+        'name': "Nicholas Sidharta",
+        'id': "2106752294",
+        'list': katalog
+    }
+
+    return render(request, 'katalog.html', data)
