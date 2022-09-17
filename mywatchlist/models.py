8f2040d868130249ce2d0b4d2eb248f316adde1d
Nicholas Sidharta <toxicancm@gmail.com>
2022-09-18 00:11:20 +0700

    heroku bug fix

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 class Watchlist(models.Model):
     watched = models.BooleanField()
     title = models.TextField()
-    rating = models.FloatField()
+    rating = models.FloatField(
+        validators=[
+            MaxValueValidator(5.0), MinValueValidator(1.0)
+            ]
+        )
     release_date = models.TextField()
     review = models.TextField()
