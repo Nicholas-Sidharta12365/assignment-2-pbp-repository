ace86f2c173ddcc2b3094c0d0a11927490e67d93
Nicholas Sidharta <toxicancm@gmail.com>
2022-09-20 13:52:50 +0700

    test

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -5,10 +5,10 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Watchlist(models.Model):
     watched = models.BooleanField()
     title = models.TextField()
-    # rating = models.FloatField(
-    #     default=1.0,
-    #     validators=[MinValueValidator(1.0), MaxValueValidator(5.0)]
+    rating = models.FloatField(
+        default=1.0,
+        validators=[MinValueValidator(1.0), MaxValueValidator(5.0)]
 
-    # )
+    )
     release_date = models.TextField()
     review = models.TextField()
